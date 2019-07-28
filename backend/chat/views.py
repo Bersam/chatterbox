25480a0f87553a74ded66e7879961baf05dc69f6
Bersam Karbasion <bersam.k@gmail.com>
2019-07-29 01:05:55 +0430

    Setup an endpoint to fetch latest messages

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from .models import Message
+from .serializers import MessageSerializer
 
 # Create your views here.
+
+class MessagesList(ListAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
