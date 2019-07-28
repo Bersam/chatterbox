9018a002017a2ca2dd6839d0cbe557d61cf15f59
Bersam Karbasion <bersam.k@gmail.com>
2019-07-28 12:59:25 +0430

    Send env variables for database and secretkey

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mj$e2#kf9@83d=wnolnm=vs*o0n=2geyuvkm5vu26bvqlae2fd'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASS': '',
+        'HOST': os.environ.get('DJANGO_DATABASE_ADDRESS'),
+        'PORT': '5432',
     }
 }
 
