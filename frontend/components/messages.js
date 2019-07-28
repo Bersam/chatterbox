ab5da39bde8298e77747e9affc780f88bc24ffa2
Bersam Karbasion <bersam.k@gmail.com>
2019-07-29 01:28:42 +0430

    make chatbox scrollable

--- frontend/components/messages.js
+++ frontend/components/messages.js
@@ -6,7 +6,7 @@ import Message from './message'
 
 
 const Messages = ({ messages }) => (
-  <Comment.Group size='big' minimal>
+  <Comment.Group size='big' minimal style={{overflow: 'auto', maxHeight: '70vh', maxWidth: '100%'}}>
     {messages.map((message) => (
       <Message message={message} key={message.timestamp} />
     ))}
