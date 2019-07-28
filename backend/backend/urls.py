85880c227d4ce4d7d3c71697990bfbb30ddd3300
Bersam Karbasion <bersam.k@gmail.com>
2019-07-29 02:36:35 +0430

    serve react on root page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls'))
+    path('chat/', include('chat.urls')),
+    path('', index)
 ]
